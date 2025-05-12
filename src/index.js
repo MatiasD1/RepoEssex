de3dbe473e61955eab6abcaf570d466767d188b0
agusgioia <agusgioia@gmail.com>
2025-05-12 17:43:31 -0300

    actualizo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Buffer } from 'buffer';
+
+// Agrega esto antes de cualquier otro código
+window.Buffer = window.Buffer || Buffer;
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 root.render(<App/>);
